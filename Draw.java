fdcb12bb745b12d732c5a299ed1c3cb0bb7faa2e
onetrueking0930 <robertjesusmbulan@gmail.com>
2019-02-26 15:30:39 +0800

    collision

--- Draw.java
+++ Draw.java
@@ -20,19 +20,68 @@ public class Draw extends JComponent{
     public BufferedImage backgroundImage;
     public URL resource = getClass().getResource("run0.png");
 
-      public int x = 300;
-      public int y = 300;
-      public int state = 0;
-    
-      public Draw(){
+      public int x = 500;
+    public int y = 300;
+    public int height = 60;
+    public int width = 490;
+    // animation states
+    public int state = 0;
+
+    //ENEMY COMPONENTS
+    // randomizer
+    public Random randomizer;
+
+    public void startGame()
+{
+        Thread gameThread = new Thread(new Runnable(){
+            public void run(){
+                while(true){
+                    try{
+                        for(int c = 0; c < monsters.length; c++){
+                            if(monsters[c]!=null){
+                                monsters[c].moveTo(x,y);
+                                repaint();
+                            }
+                        }
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                            e.printStackTrace();
+                    }
+                }
+            }
+        });
+        gameThread.start();
+    }
+          //ENEMENY COMPONENTS ^^^^^
+    public int enemyCount;
+    Monster[] monsters = new Monster[10];
+    public Draw()
+    {
+        randomizer = new Random();
+        spawnEnemy();
+
         try{
+
             image = ImageIO.read(resource);
-            backgroundImage = ImageIO.read(getClass().getResource("background.jpg"));
+            backgroundImage = ImageIO.read(getClass().getResource("BG City.jpg"));
         }
         catch(IOException e){
-            e.printStackTrace();
+            e.printStackTrace();   
+        }
+        height = image.getHeight();
+        width =  image.getWidth();
+        startGame();
+    }
+    public void spawnEnemy()
+    {
+        if(enemyCount < 10)
+        {
+            monsters[enemyCount] = new Monster(randomizer.nextInt(500), randomizer.nextInt(500), this);
+            enemyCount++;
         }
     }
+
+
     public void reloadImage(){
         state++;
         if(state == 0) {
@@ -60,7 +109,7 @@ public class Draw extends JComponent{
             catch(IOException e){
             e.printStackTrace();
         }
-        }
+      }
         public void attackAnimation(){
         Thread thread = new Thread(new Runnable(){
             public void run(){
@@ -73,16 +122,30 @@ public class Draw extends JComponent{
                             resource = getClass().getResource("attack"+ctr+".png");
                         }
                         
-                        try{
+                        try
+                        {
                             image = ImageIO.read(resource);
                         }
-                        catch(IOException e){
+                        catch(IOException e)
+                        {
                             e.printStackTrace();
                         }
                         repaint();
                         Thread.sleep(100);
-                    } catch (InterruptedException e) {
+                    }
+                     catch (InterruptedException e) 
+                    {
                         e.printStackTrace();
+                    }
+                }
+                for(int x=0; x<monsters.length; x++)
+                {
+                    if(monsters[x]!=null)
+                    {
+                        if(monsters[x].contact)
+                        {
+                            monsters[x].life = monsters[x].life - 10;
+                        }
                     }
                 }
             }
@@ -122,10 +185,10 @@ public class Draw extends JComponent{
     public void airattackAnimation(){
         Thread thread2 = new Thread(new Runnable(){
             public void run(){
-                for(int ctr = 0; ctr < 4; ctr++){
+                for(int ctr = 0; ctr < 5; ctr++){
                     try {
-                        if(ctr==3){
-                            resource = getClass().getResource("strike0.png");
+                        if(ctr==4){
+                            resource = getClass().getResource("run0.png");
                         }
                         else{
                             resource = getClass().getResource("strike"+ctr+".png");
@@ -161,26 +224,110 @@ public class Draw extends JComponent{
       y = y - 20;
       reloadImage();
       repaint();
+       checkCollision();
       }
       public void moveDown(){
       y = y + 20;
       reloadImage();
       repaint();
+       checkCollision();
       }
       public void moveLeft(){
       x = x - 20;
       reloadImage();
       repaint();
+       checkCollision();
       }
       public void moveRight(){
       x = x + 20;
       reloadImage();
       repaint();
+       checkCollision();
       }
+
+      //ENEMY SHITS
+public void checkCollision()
+{
+        int xChecker = x + width;
+        int yChecker = y;
+        for(int x=0; x<monsters.length; x++)
+        {
+            boolean collideX = false;
+            boolean collideY = false;
+            if(monsters[x]!=null)
+            {
+                monsters[x].contact = false;
+                if(yChecker > monsters[x].yPos)
+                {
+                    if(yChecker-monsters[x].yPos < monsters[x].height){
+                        collideY = true;
+                        System.out.println("collideY");
+                    }
+                }
+                else{
+                    if(monsters[x].yPos - yChecker < monsters[x].height){
+                        collideY = true;
+                        System.out.println("collideY");
+                    }
+                }
+                if(xChecker > monsters[x].xPos){
+                    if(xChecker-monsters[x].xPos < monsters[x].width){
+                        collideX = true;
+                        System.out.println("collideX");
+                    }
+                }
+                else{
+                    if(monsters[x].xPos - xChecker < 5){
+                        collideX = true;
+                        System.out.println("collideX");
+                    }
+                }
+            }
+            if(collideX && collideY){
+                System.out.println("collision!");
+                monsters[x].contact = true;
+            }
+        }
+    }
+
+    //ENEMY SHITS LAST
+        public void paintComponent(Graphics g)
+        {
+        super.paintComponent(g);
+        g.setColor(Color.YELLOW);
+        g.drawImage(backgroundImage, 0, 0, this);
+        g.drawImage(image, x, y, this);
+        //ENEMY
+        for(int c = 0; c < monsters.length; c++)
+        {
+            if(monsters[c]!=null){
+                // character grid for monsters
+                // g.setColor(Color.BLUE);
+                // g.fillRect(monsters[c].xPos, monsters[c].yPos+5, monsters[c].width, monsters[c].height);
+                g.drawImage(monsters[c].image, monsters[c].xPos, monsters[c].yPos, this);
+                g.setColor(Color.GREEN);
+                g.fillRect(monsters[c].xPos+7, monsters[c].yPos, monsters[c].life, 2);
+            }   
+        }
+    }
+    public void checkDeath(){
+        for(int c = 0; c < monsters.length; c++){
+            if(monsters[c]!=null){
+                if(!monsters[c].alive){
+                    monsters[c] = null;
+                }
+            }           
+        }
+    
+}
+
       public void paintComponent(Graphics g){
       super.paintComponent(g);
       g.setColor(Color.YELLOW);
       g.drawImage(backgroundImage, 0, 0, this);
       g.drawImage(image, x, y, this);
-      }
-}
+
+      g.drawImage(monster1.image, monster1.xPos, monster1.yPos, this);
+
+    }
+  }
--- MyFrame.java
+++ MyFrame.java
@@ -1,4 +1,4 @@
-import javax.swing.JFrame;
+/*import javax.swing.JFrame;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
@@ -61,5 +61,64 @@ public class MyFrame extends JFrame implements KeyListener {
       gameFrame.addKeyListener(gameFrame);
 
   }
+}*/
+import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MyFrame extends JFrame implements KeyListener{
+
+    Draw drawing;
+
+    public MyFrame(){
+        this.drawing = new Draw();
+    }
+
+    public void keyPressed(KeyEvent e){
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            drawing.moveUp();
+            System.out.println("Float: " + drawing.x + ", " + drawing.y);
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            drawing.moveRight();
+            System.out.println("Move Forward: " + drawing.x + ", " + drawing.y);
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            drawing.moveDown();
+            System.out.println("Slide: " + drawing.x + ", " + drawing.y);
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            drawing.moveLeft();
+            System.out.println("Move Back: " + drawing.x + ", " + drawing.y);
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_SPACE)
+        {
+            drawing.attack();
+            System.out.println("Attack");
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_S)
+        {
+            drawing.spawnEnemy();
+        }
+    }
+
+    public void keyReleased(KeyEvent e){
+
+    }
+
+    public void keyTyped(KeyEvent e){
+        
+    }
+
+    public static void main(String args[]){
+        MyFrame gameFrame = new MyFrame();
+        gameFrame.setSize(700,500);
+
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameFrame.setVisible(true);
+        gameFrame.getContentPane().add(gameFrame.drawing);
+        gameFrame.addKeyListener(gameFrame);
+        System.out.println("practical programming");
+    }
 }
 
